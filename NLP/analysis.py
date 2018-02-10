4b9ef421d6e126e2344db7b39358ae246dc9457b
Zachery DeLong <zacherypd@gmail.com>
2018-02-10 16:13:32 -0500

    Checkign in new CLI for NLP.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- NLP/analysis.py
+++ NLP/analysis.py
@@ -1,11 +1,17 @@
 import click
 
+from prototype import main
+
 @click.command()
 @click.option('--training', default='input/', help='Path to your training documents.')
 
-def analyze(training):
-    """Simple program that greets NAME for a total of COUNT times."""
+# Providing two "names" in one option creates a boolean parameter 
+@click.option('--verbose/--no-verbose', default=False, help='Print extra debugging information.')
+
+def analyze(training, verbose):
     click.echo('Training on files in {}'.format( training ) )
+
+    main(training, verbose)
 
 if __name__ == '__main__':
     analyze()
--- NLP/prototype.py
+++ NLP/prototype.py
@@ -1,40 +1,42 @@
 from gensim import corpora, models, similarities
 
-# Each element in array is considered one "document"
-documents = ["Human machine interface for lab abc computer applications",
-             "A survey of user opinion of computer system response time",
-             "The EPS user interface management system",
-             "System and human system engineering testing of EPS",
-             "Relation of user perceived response time to error measurement",
-             "The generation of random binary unordered trees",
-             "The intersection graph of paths in trees",
-             "Graph minors IV Widths of trees and well quasi ordering",
-             "Graph minors A survey"]
+def main(path_to_input, debugging = False):
+    # Each element in array is considered one "document"
+    documents = ["Human machine interface for lab abc computer applications",
+                "A survey of user opinion of computer system response time",
+                "The EPS user interface management system",
+                "System and human system engineering testing of EPS",
+                "Relation of user perceived response time to error measurement",
+                "The generation of random binary unordered trees",
+                "The intersection graph of paths in trees",
+                "Graph minors IV Widths of trees and well quasi ordering",
+                "Graph minors A survey"]
 
-# Need to initialize a new corpora from corpora.TextCorpus and initialize with lines_are_documents set to false
+    # Need to initialize a new corpora from corpora.TextCorpus and initialize with lines_are_documents set to false
 
-corpus = corpora.TextDirectoryCorpus('input/', lines_are_documents=False)
+    corpus = corpora.TextDirectoryCorpus('input/', lines_are_documents=False)
 
-# Build an array where each element is a document
-# and each document is an array of words
-# texts = [[word for word in document.lower().split()]
-#           for document in documents]
+    dictionary = corpus.dictionary
+    # Can save w/ dictionary.save()
 
-dictionary = corpus.dictionary
-# Can save w/ dictionary.save()
+    if(debugging):
+        print('Dumping tokens and respective IDs')
+        print(dictionary.token2id)
 
-print(dictionary.token2id)
+        print('Dumping corpus')
+        print(corpus)
 
-# corpus = [dictionary.doc2bow(text) for text in texts]
-# Could also save here just for giggles
-
-print(corpus)
-
-# Pass in a core and a number of topics to mine for
-model = models.ldamodel.LdaModel(corpus, 10)
+    # corpus = [dictionary.doc2bow(text) for text in texts]
+    # Could also save here just for giggles
 
 
-# To make a new prediciton, just pass in a vector to test 
-newPrediction = model[[(8, 1.0), (10, 1.0), (11, 1.0)]]
+    # Pass in a core and a number of topics to mine for
+    model = models.ldamodel.LdaModel(corpus, 10)
 
-print(newPrediction)
+
+    # To make a new prediciton, just pass in a vector to test 
+    newPrediction = model[[(8, 1.0), (10, 1.0), (11, 1.0)]]
+
+    if(debugging):
+        print('Our new prediction')
+        print(newPrediction)
