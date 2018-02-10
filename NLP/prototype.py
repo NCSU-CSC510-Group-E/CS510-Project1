79087b8b9378b86969eb330fd8c276983bb34427
Zachery DeLong <zacherypd@gmail.com>
2018-02-10 17:21:33 -0500

    Debugging info is now useful.
    Signed-off-by: Zachery DeLong <zacherypd@gmail.com>

--- NLP/prototype.py
+++ NLP/prototype.py
@@ -22,21 +22,41 @@ def main(path_to_input, debugging = False):
     if(debugging):
         print('Dumping tokens and respective IDs')
         print(dictionary.token2id)
+        print('')
 
         print('Dumping corpus')
         print(corpus)
+        print('')
 
-    # corpus = [dictionary.doc2bow(text) for text in texts]
-    # Could also save here just for giggles
+
+    # Can't figure out why id2token isn't being populated automatically.
+    # Stole this inversion code from: http://code.activestate.com/recipes/252143-invert-a-dictionary-one-liner/
+    dictionary.id2token = dict([[v,k] for k,v in dictionary.token2id.items()])
+
+    # TODO: Could also save dictionary here just for giggles
 
 
     # Pass in a core and a number of topics to mine for
-    model = models.ldamodel.LdaModel(corpus, 10)
+    # TODO: Probably ought to save this model, too, just so I dont have to load it every time from scratch
+    model = models.ldamodel.LdaModel(corpus, 10, id2word = dictionary.id2token)
 
 
-    # To make a new prediciton, just pass in a vector to test 
-    newPrediction = model[[(8, 1.0), (10, 1.0), (11, 1.0)]]
+    # To make a new prediciton, just pass in a BOW vector to test 
+    # This will return array of topics + probability tuples.  
+    newPrediction = model.get_document_topics([(8, 1.0), (10, 1.0), (11, 1.0)])
 
     if(debugging):
         print('Our new prediction')
-        print(newPrediction)
+        # print(newPrediction)
+
+        for pred in newPrediction:
+            print(pred)
+
+        print('')
+
+    # Returns the topics from the above model
+    topics = model.get_topics()
+
+    if(debugging):
+        print('The topics modeled')
+        print(model.print_topics(10))
