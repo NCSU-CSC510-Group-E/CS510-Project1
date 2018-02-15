c1d83d23265fec9b61d9dee97c282074698a7c52
zhazhangqi <zhazhangqi@gmail.com>
2018-02-15 00:59:58 -0500

    Working soparser.py to parse post.xml to Id_post.txt files.

--- NLP/SoParser.py
+++ NLP/SoParser.py
@@ -1,36 +1,51 @@
-# import xml.etree.ElementTree as et
+import xml.etree.ElementTree as et
 
-# document = et.iterparse("posts.xml", events=("start", "end"))
-# for event, elem in document:
-# 	print(elem.get('Id'))
-# 	print(elem.get('Body'))
-# 	break
-# elem.clear()
+document = et.iterparse("posts.xml", events=("start", "end"))
+i = 0
+for event, elem in document:
+    Id = elem.get('Id')
+    Body = elem.get('Body')
+    # print (type(Body))
+    if Id and Body:
+        filename = str(Id) + '_' + 'post.txt' 
+        print(filename)
+        # print (type(Body))
+        # content = unicode(Body, 'encoding')
+        f = open(filename,'w')
+        f.write(Body.encode('utf8'))
+        f.close()
+        # print(content)
+    i += 1
+
+    if i > 100:
+        break
+
+elem.clear()
 
 
-try:
-    import cElementTree as ET
-except ImportError:
-  try:
-    # Python 2.5 need to import a different module
-    import xml.etree.cElementTree as ET
-  except ImportError:
-    exit_err("Failed to import cElementTree from any known place")      
+# try:
+#     import cElementTree as ET
+# except ImportError:
+#   try:
+#     # Python 2.5 need to import a different module
+#     import xml.etree.cElementTree as ET
+#   except ImportError:
+#     exit_err("Failed to import cElementTree from any known place")      
 
-def find_in_tree(tree, node):
-    found = tree.find(node)
-    if found == None:
-        print "No %s in file" % node
-        found = []
-    return found  
+# def find_in_tree(tree, node):
+#     found = tree.find(node)
+#     if found == None:
+#         print "No %s in file" % node
+#         found = []
+#     return found  
 
-# Parse a xml file (specify the path)
-def_file = "posts.xml"
-try:
-    dom = ET.parse(open(def_file, "r"))
-    root = dom.getroot()
-except:
-    # exit_err("Unable to open and parse input definition file: " + def_file)
-    print ''
-# Parse to find the child nodes list of node 'myNode'
-fwdefs = find_in_tree(root,"Id")
+# # Parse a xml file (specify the path)
+# def_file = "posts.xml"
+# try:
+#     dom = ET.iterparse(open(def_file, "r"))
+#     root = dom.getroot()
+# except:
+#     # exit_err("Unable to open and parse input definition file: " + def_file)
+#     print 'not find'
+# # Parse to find the child nodes list of node 'Id'
+# fwdefs = find_in_tree(root,"Id")
