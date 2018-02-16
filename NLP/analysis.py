ca8e4c5f44aec82e199dceb218213274e447afcc
zpdelong <zacherypd@gmail.com>
2018-02-15 20:39:46 -0500

    Checking in work on save/load
    Signed-off-by: zpdelong <zacherypd@gmail.com>

--- NLP/analysis.py
+++ NLP/analysis.py
@@ -8,12 +8,17 @@ from prototype import main
 # Providing two "names" in one option creates a boolean parameter 
 @click.option('--verbose/--no-verbose', default=False, help='Print extra debugging information.')
 
+@click.option('--dictionary', default='save/dictionary.dict', help='The file to save your corpus dictionary to (for loading later)')
+
+@click.option('--model', default='save/model', help='The file to save your model to (for loading later)')
+
 @click.option('--test', default='input/test.txt', help='The file to predict once training is complete')
 
-def analyze(training, test, verbose):
+
+def analyze(training, test, dictionary, model, verbose):
     click.echo('Training on files in {}'.format( training ) )
 
-    main(training, test, verbose)
+    main(training, test, dictionary, model, verbose)
 
 if __name__ == '__main__':
     analyze()
--- NLP/prototype.py
+++ NLP/prototype.py
@@ -1,29 +1,60 @@
 from gensim import corpora, models, similarities
+from pathlib import Path
 
-def main(path_to_input, predictFile, debugging = False):
-    # Need to initialize a new corpora from corpora.TextCorpus and initialize with lines_are_documents set to false
-    corpus = corpora.TextDirectoryCorpus(path_to_input, lines_are_documents=False)
+def main(path_to_input, predictFile, path_to_dictionary = None, path_to_model = None,  debugging = False):
+
+    corpus = None
+    dictionary = None
+
+    d = Path(path_to_dictionary)
+    dictionaryExists = d.is_file()
+    print(dictionaryExists)
+
+    m = Path(path_to_model)
+    modelExists = m.is_file()
+    print(modelExists)
 
     # if we are given a dictionary to load, then load that instead of loading it from the corpus
-    if(path_to_dictionary is not None):
+    if(dictionaryExists):
         dictionary = corpora.Dictionary.load(path_to_dictionary)
 
+    if(not modelExists):
+        corpus = initializeCorpus(path_to_input, debugging)
+        print(corpus)
 
+    print(corpus)
 
-    # Can't figure out why id2token isn't being populated automatically.
-    # Stole this inversion code from: http://code.activestate.com/recipes/252143-invert-a-dictionary-one-liner/
+    """
+     Initialize a dictionary from our corpus
+     Can't figure out why id2token isn't being populated automatically.
+     Stole this inversion code from: http://code.activestate.com/recipes/252143-invert-a-dictionary-one-liner/
+    """
+    if(not dictionaryExists):
+        dictionary = corpus.dictionary
+        dictionary.save(path_to_dictionary)
+
     dictionary.id2token = dict([[v,k] for k,v in dictionary.token2id.items()])
 
     # TODO: Could also save dictionary here just for giggles
 
-    # Pass in a core and a number of topics to mine for
+    """
+     Train a model! (yay!)
+     Pass in a core and a number of topics to mine for
+     Providing id2word dictionary mappings here so that output of topics learned at the end are strings and not integers
+    """
+    if(not modelExists):
+        model = models.ldamodel.LdaModel(corpus, 10, id2word = dictionary.id2token, passes=40)
+        model.save(path_to_model)
+    else:
+        model = models.ldamodel.LdaModel.load(path_to_model)
+
     # TODO: Probably ought to save this model, too, just so I dont have to load it every time from scratch
-    # Providing id2word dictionary mappings here so that output of topics learned at the end are strings and not integers
-    model = models.ldamodel.LdaModel(corpus, 10, id2word = dictionary.id2token, passes=40)
 
 
-    # Lets attempt to predict a file.
-    # We first need to read that file in and tokenize it
+    """
+     Lets attempt to predict a file.
+     We first need to read that file in and tokenize it
+    """
     tokens = []
     with open(predictFile) as file:
         text = file.read()
@@ -52,3 +83,23 @@ def main(path_to_input, predictFile, debugging = False):
         print('The topics modeled')
         print(model.print_topics(10, 5))
         print('')
+
+
+def initializeCorpus(path_to_input, debugging):
+    
+    """"
+     Need to initialize a new corpora from corpora.TextCorpus and
+     initialize with lines_are_documents set to false
+    """
+    corpus = corpora.TextDirectoryCorpus(path_to_input, lines_are_documents=False)
+
+    if(debugging):
+        print('Dumping tokens and respective IDs')
+        print(dictionary.token2id)
+        print('')
+
+        print('Dumping corpus')
+        print(corpus)
+        print('')
+
+    return corpus
