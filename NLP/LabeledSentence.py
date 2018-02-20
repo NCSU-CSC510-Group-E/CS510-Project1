2742a29997484c73ee859cdc72fc8d4fdd146f60
mgmetro4 <mgmetro4@gmail.com>
2018-02-20 12:17:33 -0500

    updated label model object to handle documents

--- NLP/LabeledSentence.py
+++ NLP/LabeledSentence.py
@@ -4,9 +4,15 @@ modified to iterate through documents instead of a list
 """
 
 class LabeledLineSentence(object):
-    def __init__(self, doc_list, labels_list):
-       self.labels_list = labels_list
-       self.doc_list = doc_list
+    def __init__(self, docData, docLabels):
+        """
+        docLabels = the labels list for the documents (filenames)
+        docData = the raw contents list of the documents 
+        """
+       self.docLabels = docLabels
+       self.docData = docData
     def __iter__(self):
-        for idx, doc in enumerate(self.doc_list):
-            yield LabeledSentence(words=doc.split(),labels=[self.labels_list[idx]])
+        for idx, doc in enumerate(self.docData):
+            #split because the model is trained on a word-to-word basis
+            #could possibly tokenize here to get rid of common words
+            yield LabeledSentence(words=doc.split(),labels=[self.docLabels[idx]])
