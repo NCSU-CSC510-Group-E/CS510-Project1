4d9069b75c82cf480978473c042411d74b929e7e
mgmetro4 <mgmetro4@gmail.com>
2018-02-20 19:43:51 -0500

    fixed errors. it works maybe now

--- NLP/doc2.py
+++ NLP/doc2.py
@@ -30,29 +30,25 @@ def main():
     it = TaggedDocs(data, docLabels)
 
     #model using a fixed model rate given by gensim
-    #model = doc2vec.Doc2Vec(size=300, window=10, min_count=5, workers=11, alpha=0.025, min_alpha=0.025)
-    # NEW VERSION
-
-    #it = tagged documents iterable
-    model = doc2vec.Doc2Vec(it, size=300, window=10, min_count=5, workers=11,  alpha=0.025, min_alpha=0.025)
+    model = doc2vec.Doc2Vec(dm = 1, epochs = 10, vector_size=300, window=10, min_count=5, workers=4,  alpha=0.025, min_alpha=0.025)
     
-
     #before training, needs to build vocab
-    #model.build_vocab(it)
+    model.build_vocab(it)
 
-    #Gensim found that training over the data several times works better
-    for epoch in range(10):
-        model.train(it)
-        model.alpha -= 0.002 # decrease the learning rate over time
-        model.min_alpha = model.alpha # fix the learning rate, no decay
-        model.train(it)
+    #Gensim found that training over the data several times works better - epochs = number of iterations
+    model.train(it, total_examples=len(docLabels), epochs=model.epochs)
 
 
     # SAVE
+    print()
+    print("Saving Model..")
     model.save('model.doc2vec')
+    print("Model Saved")
+    print()
 
     # LOAD
-    model_loaded = doc2vec.Doc2Vec.load('model.doc2vec')
+    #model_loaded = doc2vec.Doc2Vec.load('model.doc2vec')
+
 
 
 
